d26867712802a37514b6192a56d3c4e24c10e906
Avani Pai <avanipai695@gmail.com>
2024-05-24 22:38:06 +0900

    adding the rest of the code from the tutorial

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+// locally defined components are camel case
+// locally references are prefixed with @
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+    return <DashboardSkeleton />;
+}
