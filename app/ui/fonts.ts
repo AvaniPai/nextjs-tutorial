d6a269f472e49c492c07c32b6d440427db2d6993
Avani Pai <avanipai695@gmail.com>
2024-06-03 22:13:45 +0900

    changed font, removed icons from toolbar, added guest-based logic to events page, added descriptions from parents to events page.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,9 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Nunito, Arapey } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin']});
 
 export const lusitana = Lusitana({ subsets: ['latin'], weight: "700"});
+
+export const nunito = Nunito({ subsets: ['latin']});
+
+export const arapey = Arapey({ weight: "400", subsets: ['latin']}); 
--- app/wedding/events/page.tsx
+++ app/wedding/events/page.tsx
@@ -1,73 +1,19 @@
 // import React from 'react';
-import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import EventBlock from '@/app/ui/events/event-block';
+import { getUserEmail } from '@/app/lib/actions';
+import { getInvitedEvents } from '@/app/lib/data';
 
-export default function Page() {
-    const events = [
-        { 
-            name: 'Mehendi',
-            date: '2024/Aug/28', 
-            description: 'Mehendi event description', 
-            imgPath: '/events/green.png',
-            bg_color: 'bg-green-200'
-        },
-        { 
-            name: 'Haldi', 
-            date: '2024/Aug/28', 
-            description: 'Haldi event description', 
-            imgPath: '/events/yellow.png',
-            bg_color: 'bg-yellow-200'
-        },
-        { 
-            name: 'Sangeet', 
-            date: '2024/Aug/30', 
-            description: 'Sangeet event description', 
-            imgPath: '/events/blue.png',
-            bg_color: 'bg-blue-300'
-        },
-        { 
-            name: 'Muhurtha', 
-            date: '2024/Aug/31', 
-            description: 'Muhurtha event description', 
-            imgPath: '/events/red.png',
-            bg_color: 'bg-pink-100'
-        },
-        { 
-            name: 'Reception', 
-            date: '2024/Aug/31', 
-            description: 'Reception event description', 
-            imgPath: '/events/catpng.png',
-            bg_color: 'bg-red-300'
-        },
-    ];
+export default async function Page() {
+    const currUserEmail = await getUserEmail();
+    const invitedEvents = await getInvitedEvents(currUserEmail);
 
     return (
         <main className="h-full">
             {/* <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Events
             </h1> */}
-            <div className="md-100 py-1 h-screen md:py-5 md:h-full" >
-                {events.map((event) => (
-                    <div key={event.name} className={`${event.bg_color} grid gap-6 grid-cols-2 h-3/6 md:grid-cols-3 mb-2 md:p-3 md:m-10`}>
-                        <div className="hidden items-center justify-center h-full bg-gray-100 md:block">
-                            {/* <Image
-                                src={event.imgPath}
-                                width={1000}
-                                height={1000}
-                                className="styles.imagehidden md:flex"
-                                alt="Screenshots of the dashboard project showing desktop version"
-                            /> */}
-                        </div>
-                        <div>
-                            <h2>{event.name}</h2>
-                            <p>Date: {event.date}</p>
-                        </div>
-                        <div>
-                            <p>Description: {event.description}</p>
-                        </div>
-                    </div>
-            ))}
-            </div>
+            <EventBlock invites={invitedEvents}/>
         </main>
     );
 }
