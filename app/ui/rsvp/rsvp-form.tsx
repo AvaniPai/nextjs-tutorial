3fead515f993d819d4c24c955139fcb9ce5c286b
Avani Pai <avanipai695@gmail.com>
2024-06-06 22:47:43 +0900

    added picture and titles to rsvp-form, removed unused images from public folder

--- app/ui/rsvp/rsvp-form.tsx
+++ app/ui/rsvp/rsvp-form.tsx
@@ -20,10 +20,10 @@ export default function RSVPForm({ invites }: { invites: InvitedEvents }) {
 
   return (
     <form action={dispatch}>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+      <div className="container rounded-md bg-pink-50 max-width mx-auto p-4 md:p-6">
         {/* Party Size */}
         <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+          <label htmlFor="amount" className="mb-2 block text-sm font-medium text-lg">
             Please enter the number of guests in your party (including yourself).
           </label>
           <div className="relative mt-2 rounded-md">
@@ -32,9 +32,8 @@ export default function RSVPForm({ invites }: { invites: InvitedEvents }) {
                 id="amount"
                 name="party_size"
                 type="number"
-                step="0.01"
                 placeholder="1"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border-none py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <UserGroupIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
--- app/wedding/rsvp/page.tsx
+++ app/wedding/rsvp/page.tsx
@@ -1,6 +1,8 @@
 import RSVPForm from '@/app/ui/rsvp/rsvp-form';
 import { getUserEmail } from '@/app/lib/actions';
 import { getInvitedEvents } from '@/app/lib/data';
+import Image from 'next/image';
+import { birthstonebounce } from '@/app/ui/fonts';
 
 export default async function Page() {
     const currUserEmail  = await getUserEmail();
@@ -8,6 +10,23 @@ export default async function Page() {
      
     return (
         <main>
+            <div className="grid grid-cols-2 grid-rows-2">
+                <div className="row-span-2">
+                    <Image 
+                    src={'/avani_and_shusuke/kanazawa_cafe.png'}
+                    width={1478}
+                    height={1108}
+                    className="md:flex size-full"
+                    alt='A picture of Avani and Shusuke in 2019, at a cafe in Kanazawa City in Ishikawa Prefecture.'
+                    />
+                </div>
+                <div className={`${birthstonebounce.className}`}>
+                    <h1 className="text-6xl text-center">We hope you can join us!</h1>
+                </div>
+                <div>
+                    <h1>Please scroll down to RSVP.</h1>
+                </div>
+            </div>
             <RSVPForm invites={invitedEvents}/>
         </main>
     )
