7b0e71d74a6ae20b26ce851997b8b33f3655007d
Avani Pai <avanipai695@gmail.com>
2024-07-19 19:56:21 +0900

    added scripts to download rsvp data, fixed rsvp bug

--- app/wedding/rsvp/page.tsx
+++ app/wedding/rsvp/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
         <main>
             <div className="bg-[url('/sakura_border.png')] bg-contain container flex flex-col justify-evenly p-4 mx-auto max-width">
                 <h1 className={` ${birthstonebounce.className} text-2xl md:text-7xl text-center md:mb-4`}>We hope you can join us!</h1>
-                <h1 className="text-xl text-center"><br/>Kindly respond by July 15<sup>th</sup>, 2024</h1>
+                <h1 className="text-xl text-center"><br/>Kindly respond by July 31<sup>st</sup>, 2024</h1>
             </div>
             <RSVPForm invites={invitedEvents}/>
         </main>
