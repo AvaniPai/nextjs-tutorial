8cf33290ac8600b97e2887727e52972dd28c7bf8
Avani Pai <avanipai695@gmail.com>
2024-07-28 22:34:37 +0900

    finished first draft of muhurhtam details page

---
+++ app/wedding/events/muhurtham/page.tsx
@@ -0,0 +1,56 @@
+import { quicksand } from '../../../ui/fonts';
+
+export default function Page(){
+	return (
+		<div className="container mx-auto justify-center text-pretty">
+			<h1 className="text-center text-4xl mb-4">The Meaning Behind Various Customs of the Hindu Wedding Ceremony</h1>
+			<p className="indent-4">Our wedding ceremony (Muhurtham) in Naperville will be performed in accordance with the Hindu traditions based on the Vedas (The Hindu Holy Scriptures) and is conducted by a priest in Sanskrit.  
+Each step of the ceremony has spiritual significance with a symbolic combination of deeply meaningful rituals. It is said that the marriage will be witnessed by the Gods in Heaven and the five elements of nature: Fire, Air, Water, Earth, and Sky. 
+Muhurtham typically involves 16 steps. Depending on the region, sub-sect, and community traditions and cultures, there will be a variation to these, sometimes skipping few and / or adding others. Here are the common steps involved in most Indian wedding ceremonies.</p>
+			<br/>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>I Baraat - Arrival of the Groom</h2>
+			<p className="indent-4 mb-4">The groom, accompanied by his family and friends arrives at the wedding venue in a procession. The entourage dances to the tune of rhythmic beating of dhol (a traditional percussion instrument) and matching music. The groom typically rides a horse, an elephant, a horse carriage or more lately in a chauffeured luxury car.  </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>II Swagatam - Greeting of the Families</h2>
+			<p className="indent-4 mb-4">Groom’s family members are formally greeted and welcomed by the bride’s family members. The groom is escorted to the traditional Mandap for commencement of ceremonial steps by the priest. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>III Ganesha Puja and Gauri PUja - Prayer to Lord Ganesha & Gauri</h2>
+			<p className="indent-4 mb-4">The wedding ceremony commences with the groom invoking and worshipping Lord Ganesha the God of wisdom and remover of obstacles. Separately, the bride performs Gauri Puja seeking blessings of goddess Gauri for a long and auspicious married life. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>IV Madhuparkam/Vara Puja - Honoring the Groom</h2>
+			<p className="indent-4 mb-4">The bride’s father washes the feet of the groom and offers him a mixture of honey, yogurt and ghee (clarified butter) as part of the traditional greeting to ensure that the new relationship will always be sweet. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>V Kanya Pravesh - Entrance of the Bride</h2>
+			<p className="indent-4 mb-4">The bride is led to the Mandap, by her maternal uncle (mother’s brother) or her father accompanied by family members along a path decorated with flower petals. A curtain (antarpaat) is held between the bride and groom to prevent them from seeing each other until the onset of the auspicious time signifying, they are still single. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>VI Kanya Daanam - Giving Away the Bride</h2>
+			<p className="indent-4 mb-4">The Kanyadaanam is part of the marriage ceremony in which Bride’s family represented by her father gives her away to the groom. The priest invokes the blessings of the ancestors belonging to three generations of both families. The bride’s parents place the bride’s hands in the groom’s hands and ask him to accept her as his equal partner in life. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>VII Shubhamurhurtham (Jeelakarra Bellam) - Auspicious Moment</h2>
+			<p className="indent-4 mb-4">At the pre-set, precise auspicious moment called Shubhamuhurtham, the bride and groom apply a paste of cumin seeds (jeera) and jaggery on each other's heads, while the cloth curtain is still upheld. When ground together, the cumin and sweet jaggery turn into an inseparable mixture signifying that the bride and groom are supposed to become inseparable throughout life's journey. The cloth curtain is then removed, unveiling the "new" life in holy matrimony with the bride and groom looking at each other formally for the first time. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>VIII Varmala - Exchange of garlands</h2>
+			<p className="indent-4 mb-4">The bride and the groom exchange floral garlands symbolizing their acceptance of the union and a promise to lead the rest of their lives as husband and wife. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>IX Mangalsutra Dharanam - Tying of Matrimonial Necklace</h2>
+			<p className="indent-4 mb-4">The groom then places a ceremonial necklace around bride’s neck symbolizing the bonding of their marriage. The necklace is called Mangala Sutra which is made of gold with coral stones as a symbol of their hearts, minds, and soul blending together in life. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>X Talambralu - Pouring of Sacred Saffron Rice and Pearls</h2>
+			<p className="indent-4 mb-4">The bride and groom then shower each other's heads with rice mixed with saffron. This tradition of pouring Mutyala Talambralu denotes the couple's desire for bountifulness, happiness, enjoyment, and contentment. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>XI Paani Grahanam & Granthi Bhandhanam - Holding Hnads & Tying of Sacred Knot</h2>
+			<p className="indent-4 mb-4">The groom holds the bride’s right hand firmly, grasping all five fingers in his right palm while declaring acceptance of each other in the presence of God. The sacred knot is tied with the scarves worn by them. The knot signifies their eternal love and commitment to each other. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>XII Pradhana Homam & Laaja Homam - Invoking the God of Fire & Offering to Sacred Fire</h2>
+			<p className="indent-4 mb-4">The newly wedded couple offer prayer to Agni (the Lord of Fire), who then becomes the witness to the wedding. Puffed rice is offered to Agni for happiness and prosperity in their life together. The couple, thus united, walk around the holy fire four times, symbolizing the righteous walk of life with Dharma (duty), Artha (prosperity), Kama (Desire), and Moksha (Liberation), the 4 aims of humans in Hindu philosophy, that are essential for achieving a balanced and fulfilling life. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>XIII Saptapadi - &quot;Seven Steps&quot;</h2>
+			<p className="indent-4 mb-4">The couple perform the most important rite of the entire ceremony by taking seven steps together, symbolizing the beginning of their journey through life as partners. At the beginning of each step, the groom repeats a Vedic mantra to invoke the blessings of Lord Vishnu. These seven steps reflect their guiding principles in life: 
+			<ol className="list-decimal">
+				<li>To provide for a pure household, including a clean and nourishing diet. </li>
+				<li>To develop physical, mental, and spiritual strengths. </li>
+				<li>To increase and preserve wealth by righteous and proper means. </li>
+				<li>To develop harmony by mutual love, respect, and happiness. </li>
+				<li>To be blessed with strong, virtuous, and healthy children. </li>
+				<li>To practice righteousness and obtain longevity.       </li>
+				<li>To be true companions and remain life-long partners.  </li>
+			</ol>
+
+Upon completion of the seven steps, the bride is asked to sit beside the groom. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>XIV Shilaroohanam & Mattelu - Placing Toe Rings</h2>
+			<p className="indent-4 mb-4">The bride is asked to step on a stone signifying that she be rock-firm and unperturbed by the trials and tribulations of life. Then the bride’s toes are adorned with toe rings signifying her status as a married woman. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>XV Vasant Madhav Pooja - Finding the Ring</h2>
+			<p className="indent-4 mb-4">The newly married couple compete to find a ring from a large bowl filled with milk, water and flower petals. The successful ring finder signifies the dominant person in the relationship and the one who rules the house. </p>
+			<h2 className={`${quicksand.className} text-xl mb-2`}>XVI Aashirvaadam - Showering of Blessings</h2>
+			<p className="indent-4 mb-4">The newly married couple, seek the blessings of the priest and the elders. All those present shower them with rice and flower petals. </p>
+		</div>
+	);
+}
